aad3605aefc113b2ee5f2d6d00796fc88e4399e9
datistpham <giang10a1dz@gmail.com>
2023-05-03 00:45:34 +0700

    commit final

--- src/api/add_stage_item.js
+++ src/api/add_stage_item.js
@@ -1,12 +1,12 @@
 import axios from "axios"
 import { API_URL } from "../config"
 
-const add_stage_item= async (device, mode, startPoint, endPoint, state, timeStart, timeEnd, stage_id)=> {
+const add_stage_item= async (device, mode, startPoint, endPoint, state, timeStart, timeEnd, stage_id, date)=> {
     const res= await axios({
         url: API_URL+ "/api/add-stage/item",
         method: "post",
         data: {
-            device, mode, startPoint, endPoint, state, timeStart, timeEnd, stage_id
+            device, mode, startPoint, endPoint, state, timeStart, timeEnd, stage_id, date
         }
     })
     const result= await res.data
--- src/components/Stage.js
+++ src/components/Stage.js
@@ -14,6 +14,27 @@ import { API_URL } from "../config";
 import DetailStage from "./Stage/DetailStage";
 import DeleteIcon from '@mui/icons-material/Delete';
 import delete_stage from "../api/delete_stage";
+import { Button } from "@mui/material";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+// import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+// import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import Slider from "@mui/material/Slider";
+import Switch from '@mui/material/Switch';
+import {  DatePicker, TimePicker } from 'antd';
+import add_stage_item from "../api/add_stage_item";
+import moment from "moment";
+import _ from "lodash"
+
+// import edit_stage_item from "../../api/edit_stage_item";
+
 const Stage = () => {
   const [listStage, setListStage] = React.useState([]);
   const [change, setChange]= React.useState(false)
@@ -90,10 +111,11 @@ const StageItem = (item) => {
         <AccordionDetails>
           <Typography>
             {
-              data?.map((item2, key)=> <DetailStage setChange2={setChange} setChange={item?.setChange} key={key} index={parseInt(key)} {...item2} />)
+              _.orderBy(data, (e)=> moment(e.date).valueOf(), "asc")?.map((item2, key)=> <DetailStage setChange2={setChange} setChange={item?.setChange} key={key} index={parseInt(key)} {...item2} />)
             }
           </Typography>
         </AccordionDetails>
+        <AddStageItem setChange2={setChange} stage_id={item?.stage_id} />
       </Accordion>
       <br />
     </React.Fragment>
@@ -101,3 +123,125 @@ const StageItem = (item) => {
 };
 
 export default Stage;
+
+
+
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+function AddStageItem(props) {
+  const [date, setDate]= React.useState()
+  const [stageItemId, setStageItemId]= React.useState("")
+  const [device, setDevice] = React.useState("");
+  const [mode, setMode] = React.useState("");
+  const [startPoint, setStartPoint] = React.useState(20);
+  const [endPoint, setEndPoint] = React.useState(37);
+  const [timeStart, setTimeStart] = React.useState();
+  const [timeEnd, setTimeEnd] = React.useState();
+  const [state, setState] = React.useState(0);
+  const [stageId, setStageId] = React.useState(props?.stage_id);
+  const [open, setOpen] = React.useState(false)
+  const handleClick= () => {
+    setOpen(true)
+  }
+  const handleClose= () => {
+      setOpen(false)
+    }
+  return (
+    <>
+      <Button onClick={handleClick} color={"primary"} variant={"contained"} style={{width: "100%"}}>Thêm ngày</Button>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{"Thêm ngày"}</DialogTitle>
+        <DialogContent>
+          <DatePicker onChange={(date, dateString)=> {
+            setDate(date?.format("DD/MM/YYYY"))
+          }} />
+          <FormControl fullWidth style={{marginTop: 12}}>
+            <InputLabel id="demo-simple-select-label">Thiết bị</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={device}
+              label="Thiết bị"
+              onChange={(e)=> setDevice(e.target.value)}
+            >
+              <MenuItem value={1}>Máy bơm</MenuItem>
+              <MenuItem value={2}>Quạt</MenuItem>
+              <MenuItem value={3}>Đèn</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl fullWidth style={{marginTop: 12}}>
+            <InputLabel id="demo-simple-select-label">Chế độ</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={mode}
+              label="Chế độ"
+              onChange={(e)=> setMode(e.target.value)}
+            >
+              <MenuItem value={1}>Độ ẩm</MenuItem>
+              <MenuItem value={2}>Nhiệt độ</MenuItem>
+              <MenuItem value={3}>Ánh sáng</MenuItem>
+            </Select>
+          </FormControl>
+          <Slider
+            getAriaLabel={() => "Humility range"}
+            value={[startPoint, endPoint]}
+            onChange={(e, value)=> {
+              setStartPoint(value[0])
+              setEndPoint(value[1])
+            }}
+            valueLabelDisplay="auto"
+          />
+          <div className={"c-flex-center"} style={{justifyContent: "flex-start", gap: 10}}>
+          <Typography>Tắt</Typography>
+          <Switch
+              checked={state=== 1 ? true : false}
+              onChange={(e)=> {
+                if(e.target.checked) {
+                  setState(1)
+                }
+                else {
+                  setState(0)
+                }
+              }}
+              inputProps={{ 'aria-label': 'controlled' }}
+
+          />
+          <Typography>Bật</Typography>
+          </div>
+          <TimePicker.RangePicker onChange={(time)=> {
+            setTimeStart(time[0].format("HH:mm:ss"))
+            setTimeEnd(time[1].format("HH:mm:ss"))
+          }} />
+        </DialogContent>
+
+
+
+        <DialogActions>
+          <Button onClick={()=> props?.handleClose()}>Đóng</Button>
+          <Button onClick={async ()=> { 
+            const result= await add_stage_item(device, mode, startPoint, endPoint, state, timeStart, timeEnd, stageId, date)
+            if(result?.add=== true) {
+              handleClose()
+              props?.setChange2(prev=> !prev)
+            }
+            props?.handleClose()
+          }}>Tạo</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
+
+// const ComponentEditDetailStage = (props) => {
+//   return <div></div>;
+// };
